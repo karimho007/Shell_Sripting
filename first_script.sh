d6beeeda8c9ef8610c247fb2bbda32654ca06874
karimho007 <bisri.karim@gmail.com>
2021-09-18 23:03:35 -0700

    Version 1.0

---
+++ first_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# this is the first script
+# Scriptname : xxx
+# Written by : developper name
+
+echo "Hello $LOGNAME, Have a nice day !"
+echo "You are working in directory `pwd`."
+echo "List of files in your directory is :"
+ls #List all files in the present working directory
+echo "Bye for now $LOGNAME. The time is `date +%T` !"
